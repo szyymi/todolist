df2823eec284b2dcf643ce5fb68ba945d4bd3daa
Szymon Dobosz <109244081+szyymi@users.noreply.github.com>
2023-06-20 23:40:27 +0200

    Add files via upload
    Added new features

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const todoList = document.querySelector('.todo-list');
 
 // Event Listeners
 todoButton.addEventListener("click", addTodo);
-
+todoList.addEventListener("click", deleteCheck);
 // Functions
 
 function addTodo(event) {
@@ -17,7 +17,7 @@ const todoDiv = document.createElement("div");
 todoDiv.classList.add("todo");
 // Create li 
 const newTodo = document.createElement("li");
-newTodo.innerText = "hey";
+newTodo.innerText = todoInput.value;
 newTodo.classList.add("todo-item");
 todoDiv.appendChild(newTodo);
 
@@ -32,9 +32,28 @@ todoDiv.appendChild(completedButton);
 
 const trashButton = document.createElement('button');
 trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-trashButton.classList.add("complete-btn");
+trashButton.classList.add("trash-btn");
 todoDiv.appendChild(trashButton);
 
 //  Append to the list 
 todoList.appendChild(todoDiv);
+//Clear to do value
+todoInput.value = " ";
+}
+
+function deleteCheck(e){
+const item = e.target;
+//Delete todo
+if(item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function(){
+todo.remove();
+    });
+}
+//Check mark completed
+if(item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+}
 }
